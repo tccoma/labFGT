d2042d7947668e8cfe23472264bf93225deaab7d
tccome <tccoma@gmail.com>
2025-03-23 16:58:33 +1100

    initial commit

---
+++ main.tf
@@ -0,0 +1,30 @@
+#this is a test
+
+terraform {
+  required_providers {
+    fortios = {
+      source  = "fortinetdev/fortios"
+    }
+  }
+}
+
+provider "fortios" {
+  hostname     = "220.233.67.203:10443"
+  token        = "bGk7hxmnkqhqygz3hnqpqtzdpwGkGp"
+  insecure     = "true"
+}
+
+
+
+resource "fortios_system_dns" "trname" {
+  cache_notfound_responses = "disable"
+  dns_cache_limit          = 5000
+  dns_cache_ttl            = 1800
+  ip6_primary              = "::"
+  ip6_secondary            = "::"
+  primary                  = "8.8.8.8"
+  retry                    = 2
+  secondary                = "8.8.4.4"
+  source_ip                = "0.0.0.0"
+  timeout                  = 5
+}
